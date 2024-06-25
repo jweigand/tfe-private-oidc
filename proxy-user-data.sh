3fb3c8cb8728e5e3ce95e832c16198817ca794b3
John Weigand <john.weigand@hashicorp.com>
2024-06-25 16:55:34 -0400

    fix listen-address replace

--- proxy-user-data.sh
+++ proxy-user-data.sh
@@ -5,5 +5,5 @@ sudo apt-get update
 sudo apt-get install privoxy -y
 
 sudo ip=$(ec2metadata --local-ipv4)
-sudo sed -i "s/192.168.0.1/$ip/g" /etc/privoxy/config
+sudo sed -i "s/127.0.0.1/$ip/g" /etc/privoxy/config
 sudo /etc/init.d/privoxy restart
