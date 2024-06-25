d165b6b2190279148a6bc7f1b737213d1dfcf602
John Weigand <john.weigand@hashicorp.com>
2024-06-25 15:24:54 -0400

    proxy sg

--- proxy-user-data.sh
+++ proxy-user-data.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+#https://help.ubuntu.com/community/Privoxy
 sudo apt-get update
 sudo apt-get install privoxy -y
--- proxy.tf
+++ proxy.tf
@@ -23,19 +23,51 @@ data "aws_iam_role" "ssm" {
   name = "AmazonSSMRoleForInstancesQuickSetup"
 }
 
+data "aws_subnet" "this" {
+  id = var.proxy_subnet_id
+}
+
+data "aws_vpc" "this" {
+  id = data.aws_subnet.this.vpc_id
+}
+
 resource "aws_instance" "proxy" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = "t3.micro"
   associate_public_ip_address = false
   iam_instance_profile        = aws_iam_instance_profile.proxy.name
-  subnet_id                   = var.proxy_subnet_id
-  #vpc_security_group_ids = []
+  subnet_id                   = data.aws_subnet.this.id
+  vpc_security_group_ids      = [aws_security_group.proxy.id]
   user_data_replace_on_change = true
   user_data                   = file("${path.module}/proxy-user-data.sh")
 
   tags = {
     Name = "proxy"
   }
+}
+
+resource "aws_security_group" "proxy" {
+  name_prefix = "proxy"
+  description = "Allow inbound traffic on port 80 and 443"
+  vpc_id      = data.aws_subnet.this.vpc_id
+}
+
+resource "aws_security_group_rule" "proxy_ingress" {
+  type              = "ingress"
+  from_port         = 8188
+  to_port           = 8188
+  protocol          = "tcp"
+  security_group_id = aws_security_group.proxy.id
+  cidr_blocks       = [data.aws_vpc.this.cidr_block]
+}
+
+resource "aws_security_group_rule" "proxy_egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  security_group_id = aws_security_group.proxy.id
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
 variable "proxy_subnet_id" {
