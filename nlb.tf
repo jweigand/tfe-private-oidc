9d6e6e8e628b818a575afa5ebe9935e27a109635
John Weigand <john.weigand@hashicorp.com>
2024-06-26 13:08:37 -0400

    nlb to alb

--- nlb.tf
+++ nlb.tf
@@ -39,6 +39,31 @@ resource "aws_lb_listener" "proxy" {
   }
 }
 
+resource "aws_lb_listener" "nlb_https" {
+  load_balancer_arn = aws_lb.tfe_nlb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb.arn
+  }
+}
+
+resource "aws_lb_target_group" "alb" {
+  name        = "nlb-to-alb"
+  port        = 443
+  protocol    = "TCP"
+  vpc_id      = data.aws_vpc.this.id
+  target_type = "alb"
+}
+
+resource "aws_lb_target_group_attachment" "alb" {
+  target_group_arn = aws_lb_target_group.alb.arn
+  target_id        = aws_lb.alb.arn
+  port             = 443
+}
+
 resource "aws_security_group" "nlb" {
   name_prefix = "tfe_nlb"
   vpc_id      = data.aws_vpc.this.id
