9bd72a2bfeb1882f4b2ff1055376c9c01607f5b8
John Weigand <john.weigand@hashicorp.com>
2024-06-26 12:48:51 -0400

    fix cert zone

--- dns_cert.tf
+++ dns_cert.tf
@@ -33,7 +33,7 @@ resource "aws_route53_record" "cert_validation" {
   name            = tolist(aws_acm_certificate.tfe.domain_validation_options)[0].resource_record_name
   records         = [tolist(aws_acm_certificate.tfe.domain_validation_options)[0].resource_record_value]
   type            = tolist(aws_acm_certificate.tfe.domain_validation_options)[0].resource_record_type
-  zone_id         = aws_lb.tfe_nlb.zone_id
+  zone_id         = data.aws_route53_zone.this.zone_id
   ttl             = 60
 }
 
