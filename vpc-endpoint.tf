969c46226c98306a7c79c3835eb80eff2b8ff421
John Weigand <john.weigand@hashicorp.com>
2024-06-26 16:53:12 -0400

    fix id

--- vpc-endpoint.tf
+++ vpc-endpoint.tf
@@ -3,9 +3,7 @@ resource "aws_vpc_endpoint" "api" {
   service_name      = "com.amazonaws.${var.aws_region}.execute-api"
   vpc_endpoint_type = "Interface"
 
-  security_group_ids = [
-    aws_security_group.api_endpoint,
-  ]
+  security_group_ids = [aws_security_group.api_endpoint.id]
 
   subnet_ids = var.public_subnet_ids
 
