211f040e0bdd9faf42df1ba3b7a3ea88dce02f73
John Weigand <john.weigand@hashicorp.com>
2024-06-27 08:54:41 -0400

    sg fixes

--- nlb.tf
+++ nlb.tf
@@ -100,6 +100,15 @@ resource "aws_security_group_rule" "nlb_proxy_ingress" {
   cidr_blocks       = [for ip in data.aws_nat_gateway.this.*.public_ip : format("%s/32", ip)]
 }
 
+resource "aws_security_group_rule" "nlb_https_ingress" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  security_group_id = aws_security_group.nlb.id
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
 resource "aws_security_group_rule" "nlb_egress" {
   type              = "egress"
   from_port         = 0
--- vpc-endpoint.tf
+++ vpc-endpoint.tf
@@ -14,6 +14,24 @@ resource "aws_security_group" "api_endpoint" {
   vpc_id = data.aws_vpc.this.id
 }
 
+resource "aws_security_group_rule" "api_https_ingress" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  security_group_id = aws_security_group.api_endpoint.id
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "api_egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  security_group_id = aws_security_group.api_endpoint.id
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
 data "aws_network_interface" "api" {
   for_each = toset(aws_vpc_endpoint.api.network_interface_ids)
   id       = each.value
